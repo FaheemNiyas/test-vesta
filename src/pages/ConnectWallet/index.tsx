1f83052b1f8baa50d7cb472085f8539a68d0566f
Rikas <rikasrkf@gmail.com>
2024-07-19 17:05:53 +0530

    fix: wallet connect

--- src/pages/ConnectWallet/index.tsx
+++ src/pages/ConnectWallet/index.tsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import OptionalCard from "@/components/organisms/OptionalCard";
 import AuthLayout from "@/layouts/AuthLayout";
 import { useNavigate } from "react-router-dom";
+import { useAccount } from "wagmi";
 
 const ConnectWallet: React.FC = () => {
   const navigate = useNavigate();
+  const { isConnected } = useAccount();
+
+  useEffect(() => {
+    if (isConnected) {
+      navigate("/questionary");
+    }
+  }, [isConnected, navigate]);
 
   return (
     <div className="relative h-min-screen pt-30">
@@ -20,7 +28,7 @@ const ConnectWallet: React.FC = () => {
           onSkipNow={() => navigate("/questionary")}
         >
           <div className="flex items-center justify-center py-4">
-            <w3m-button label="Connect Wallet" />
+            <w3m-button balance="show" label="Connect Wallet" />
           </div>
         </OptionalCard>
       </AuthLayout>
